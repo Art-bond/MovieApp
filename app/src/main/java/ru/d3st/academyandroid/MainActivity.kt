a5d3ca7e2106505b9379de38a92a29d5c9747b6e
Artem <d3miui@gmail.com>
2020-11-22 17:16:38 +0400

    make up layout MovieDetailsActivity.kt

--- app/src/main/java/ru/d3st/academyandroid/MainActivity.kt
+++ app/src/main/java/ru/d3st/academyandroid/MainActivity.kt
@@ -1,11 +1,20 @@
 package ru.d3st.academyandroid
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn = findViewById<Button>(R.id.to_movie_detail_button)
+        btn.setOnClickListener {
+            val intent = Intent(this, MovieDetailsActivity::class.java)
+
+            startActivity(intent)
+        }
     }
 }
