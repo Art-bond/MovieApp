109f89c38a898557be2d2f711ac2b414b3cc886e
Artem <d3miui@gmail.com>
2020-12-05 18:26:25 +0400

    fix equal spacing between items in gridLayout RecyclerView add navigation on back button

--- app/src/main/java/ru/d3st/academyandroid/overview/MovieListFragment.kt
+++ app/src/main/java/ru/d3st/academyandroid/overview/MovieListFragment.kt
@@ -1,13 +1,18 @@
 package ru.d3st.academyandroid.overview
 
+import android.content.res.Configuration
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import ru.d3st.academyandroid.R
 import ru.d3st.academyandroid.databinding.FragmentMoviesListBinding
+
 
 class MovieListFragment : Fragment() {
 
@@ -29,7 +34,7 @@ class MovieListFragment : Fragment() {
 
 
         // Inflate the layout for this fragment
-        _bind = FragmentMoviesListBinding.inflate(inflater,container, false)
+        _bind = FragmentMoviesListBinding.inflate(inflater, container, false)
 
 
         //lifecycleOwner управляет жизненным циклом фрагмента
@@ -37,26 +42,55 @@ class MovieListFragment : Fragment() {
         //получаем Viewmodel
         bind.movieListViewModel = viewModel
 
-        val adapter = MovieListAdapter(MovieListAdapter.ClickListener{ movie ->
+
+        val adapter = MovieListAdapter(MovieListAdapter.ClickListener { movie ->
             viewModel.displayMovieDetails(movie)
         })
+
         bind.rvMoviesList.adapter = adapter
 
-        viewModel.movieList.observe(viewLifecycleOwner,{
-            it?.let{
+        //bind.rvMoviesList.addItemDecoration(GridSpacingItemDecoration(10))
+
+        //Так как Превью фильма в списке имеет фиксированное значение,
+        //получаем его ширину в пикселях исходя из DP данного устройства
+        val itemWidth = resources.getDimensionPixelSize(R.dimen.recycler_view_item_width)
+
+
+        val orientation: Int = resources.configuration.orientation
+        //в зависимости от ориентации будет выбираться количество столбцов либо 2 либо 4
+        val spanCount = if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            //in landscape
+            4
+        } else {
+            // In portrait
+            2
+        }
+
+        val manager = GridLayoutManager(
+            activity,
+            spanCount
+        )
+        bind.rvMoviesList.layoutManager = manager
+        //настраиваем равные отступы и центрирование элементов в нашем списке(сетке)
+        bind.rvMoviesList.addItemDecoration(GridSpacingItemDecoration(spanCount, itemWidth))
+
+        viewModel.movieList.observe(viewLifecycleOwner, {
+            it?.let {
                 adapter.submitList(it)
             }
         })
 
-        viewModel.navigateToSelectedMovie.observe(viewLifecycleOwner,{
+
+        viewModel.navigateToSelectedMovie.observe(viewLifecycleOwner, {
             //проверяем есть ли данные фильма
-            if (it != null){
+            if (it != null) {
                 //вызываем финдНавКонтроллер
                 this.findNavController().navigate(
                     //после подключения SafeArgs
-                MovieListFragmentDirections.actionListToDetail(it))
-                        //приводим пеерменную отвечающую за переход в исходное состояние
-                        viewModel.displaySelectedGroupComplete()
+                    MovieListFragmentDirections.actionListToDetail(it)
+                )
+                //приводим пеерменную отвечающую за переход в исходное состояние
+                viewModel.displaySelectedGroupComplete()
             }
         })
 
